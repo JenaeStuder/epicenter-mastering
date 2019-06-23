ae2fac122d4a2fe17a6410378dfc296f3c5243e6
Jenae <Jenaenay15@yahoo.com>
2019-06-23 12:15:37 -0700

    added individual css pages for each html page, changed the animation on logo on contact page, updated cdn's

---
+++ assets/contact.js
@@ -0,0 +1,11 @@
+// Javascript for contact page 
+
+// ***makes the image spin horizontally***
+
+// $(document).ready(function(){
+//     $("#imageLoading").addClass("imageRotateHorizontal");
+//   });
+  
+  // $(".button").on('click', function(){
+  //   $("#imageLoading").removeClass("imageRotateHorizontal");
+  // });
